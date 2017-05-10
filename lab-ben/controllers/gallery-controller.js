72c0556d2b4c0764243f58e6d4aaeeb09fe9cdfa
Benjamin <ayzenbergb@gmail.com>
2017-05-10 10:45:28 -0700

    gallery-routes done but not tested

--- lab-ben/controllers/gallery-controller.js
+++ lab-ben/controllers/gallery-controller.js
@@ -7,11 +7,15 @@ const debug = require('debug')('cfgram:gallery-controller');
 module.exports = exports = {};
 
 exports.addGallery = function(req) {
+  debug('gallery-controller addGallery');
+
   req.body.userId = req.user_id;
   return new Gallery(req.body).save();
 };
 
 exports.findGallery = function(id) {
+  debug('gallery-controller findGallery');
+
   Gallery.findById(id)
   .then(gallery => {
     if(gallery.userId.toString() !== id) {
@@ -19,4 +23,17 @@ exports.findGallery = function(id) {
     }
     return gallery;
   });
+};
+
+exports.updateGallery = function(id, body) {
+  Gallery.findByIdAndUpdate(id, {$set: {
+    name: body.name,
+    desc: body.desc,
+    created: body.created,
+  }}).save()
+  .catch(err => err);
+};
+
+exports.removeGallery = function(id) {
+  Gallery.remove({id: id});
 };
--- lab-ben/routes/gallery-routes.js
+++ lab-ben/routes/gallery-routes.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const createError = require('http-errors');
-const Gallery = require('../models/gallery.js');
 const galleryCtrl = require('../controllers/gallery-controller.js');
 const debug = require('debug')('cfgram:gallery-routes');
 const bearerAuth = require('../lib/bearer-auth-middleware.js');
@@ -16,15 +15,24 @@ module.exports = function(router) {
   });
 
   router.get('/gallery/:id', bearerAuth, (req, res) => {
-    debug('#GET /api/gallery/:id');
+    debug('#GET /api/gallery');
 
-    Gallery.findbyId(req.params.id)
-    .then(gallery => {
-      if(gallery.userId.toString() !== req.user._id.toString()) {
-        return createError(401, 'Invalid user');
-      }
-      res.json(gallery);
-    })
+    galleryCtrl.findGallery(req.params.id)
+    .then(gallery => res.json(gallery))
+    .catch(err => res.status(err.status).send(err.message));
+  });
+
+  router.put('/api/gallery/:id', bearerAuth, (req, res) => {
+    debug('#PUT /api/gallery');
+
+    galleryCtrl.updateGallery(req.params.id, req.body)
+    .then(gallery => res.json(gallery))
+    .catch(err => res.status(err.status).send(err.message));
+  });
+
+  router.delete('/api/gallery/:id', bearerAuth, (req, res) => {
+    galleryCtrl.removeGallery(req.params.id)
+    .then(() => res.json('item removed'))
     .catch(err => res.status(err.status).send(err.message));
   });
   return router;
